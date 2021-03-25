697cbae45b1154d113a57a4bd05154f48134ff09
takuma1192 <78262525+takuma1192@users.noreply.github.com>
2021-03-25 21:45:39 +0900

    Add files via upload

--- src/components/CommonHeader.vue
+++ src/components/CommonHeader.vue
@@ -13,13 +13,51 @@
             </ul>
           </nav>
         </div>
-        <div id="hamburger">
+        <div id="hamburger" @click="hamburger()" :class="{'active':isClass}">
           <span></span>
           <span></span>
           <span></span>
         </div>
+        <nav class="menu-content" :class="{'open':isClass}">
+          <h1 class="menu-title">estra inc.</h1>
+          <ul class="menu-content_inner">
+            <li>
+              <a href="#">home</a>
+            </li>
+            <li>
+              <a href="#">about</a>
+            </li>
+            <li>
+              <a href="#">service</a>
+            </li>
+            <li>
+              <a href="#">works</a>
+            </li>
+            <li>
+              <a href="#">price</a>
+            </li>
+            <li>
+              <a href="">contact</a>
+            </li>
+          </ul>
+        </nav>
       </header>  
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      isClass: false
+    };
+  },
+  methods: {
+    hamburger() {
+      this.isClass = !this.isClass;
+    }
+  }
+};
+</script>
 
 <style scoped>
 a {
@@ -101,6 +139,61 @@ a {
     position: absolute;
     top: 30px;
     left: 2px;
+  }
+
+  #hamburger.active span:nth-of-type(1) {
+    top: 20px;
+    transform: rotate(45deg);
+  }
+
+  #hamburger.active span:nth-of-type(2) {
+    opacity: 0;
+  }
+
+  #hamburger.active span:nth-of-type(3) {
+    top: 20px;
+    transform: rotate(-45deg);
+  }
+
+  .menu-content {
+    display: block;
+    width: calc(100% - 80px);
+    height: 100%;
+    text-align: center;
+    transition: 0.2s;
+    position: fixed;
+    top: 0;
+    left: calc(-100% - 80px);
+    background: #fff;
+    color: #333333;
+    box-shadow: 80px 0 rgba(38, 98, 213, 0.3);
+    z-index: 999;
+  }
+
+  .menu-content .menu-title {
+    margin: 40px 20px;
+    font-size: 2.2rem;
+    color: #333333;
+    text-align: left;
+    font-weight: bold;
+  }
+
+  .menu-content li a {
+    color: #333333;
+    margin: 15px;
+    padding: 5px;
+    boeder-bottom: 0.5px solid #2662d5;
+    text-decoration: none;
+    display: block;
+  }
+
+  .menu-content li {
+    width: 100%;
+    text-align: left;
+  }
+
+  .menu-content.open {
+    left: 0;
   }
 }  
 </style>
